fe4c230fc2d41d72ecc52213fff84302f60ae044
Allan8606 <allanisaac.dev@gmail.com>
2026-09-17 06:01:42 -0300

    feat: implementa service da entidade Technology, e finaliza todos os services.

--- devshowcase/devshowcase/src/main/java/com/devshowcase/service/TechnologyService.java
+++ devshowcase/devshowcase/src/main/java/com/devshowcase/service/TechnologyService.java
@@ -1,4 +1,69 @@
 package com.devshowcase.service;
 
+
+import com.devshowcase.dto.request.TechnologyRequestDTO;
+import com.devshowcase.dto.response.TechnologyResponseDTO;
+import com.devshowcase.entity.Technology;
+import com.devshowcase.repository.TechnologyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TechnologyService {
+    private final TechnologyRepository technologyRepository;
+
+    //Cadastrar
+    public TechnologyResponseDTO cadastrar(TechnologyRequestDTO request){
+        Technology technology = converteParaEntity(request);
+        return converteParaResponse(technologyRepository.save(technology));
+    }
+
+    //Listar todos
+    public List<TechnologyResponseDTO> listarTodos(){
+        List<TechnologyResponseDTO> list = technologyRepository.findAll()
+                .stream()
+                .map(technology -> converteParaResponse(technology))
+                .toList();
+        return list;
+    }
+
+    //Listar Por ID
+    public TechnologyResponseDTO buscarPorid(Long id){
+        Technology technology = technologyRepository.findById(id).orElseThrow();
+        return converteParaResponse(technology);
+    }
+
+    //Editar
+    public TechnologyResponseDTO editar(Long id, TechnologyRequestDTO request){
+        Technology technology = technologyRepository.findById(id).orElseThrow();
+        technology.setName(request.name());
+
+        Technology save = technologyRepository.save(technology);
+
+        return converteParaResponse(save);
+    }
+
+    //Deletar
+    public void deletar(Long id){
+        Technology technology = technologyRepository.findById(id).orElseThrow();
+        technologyRepository.delete(technology);
+    }
+
+    //====================Métodos auxiliares===============
+    private Technology converteParaEntity(TechnologyRequestDTO request){
+        return new Technology(
+                request.name()
+        );
+    }
+
+    private TechnologyResponseDTO converteParaResponse(Technology technology){
+        return new TechnologyResponseDTO(
+                technology.getId(),
+                technology.getName()
+        );
+    }
+
 }
